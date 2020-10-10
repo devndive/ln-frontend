940fccd7905fe992fb34824aecfb446b621133e6
Yann Duval <yann.duval@posteo.de>
2020-10-10 19:40:54 +0200

    Added validation to login screen

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import { gql, useMutation } from "@apollo/client";
 import { useHistory, useLocation } from "react-router-dom";
+import classnames from 'classnames';
+import { useForm } from 'react-hook-form';
 
 export const LOGIN_MUTATION = gql`
   mutation Login($email: String!, $password: String!) {
@@ -12,9 +14,6 @@ export const LOGIN_MUTATION = gql`
 `;
 
 export const Login = ({ setIsAuthenticated }: { setIsAuthenticated: (state: boolean) => void }) => {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-
   const [isLoading, setIsLoading] = React.useState(false);
 
   const [error, setError] = React.useState("");
@@ -24,8 +23,10 @@ export const Login = ({ setIsAuthenticated }: { setIsAuthenticated: (state: bool
   const history = useHistory();
   const location = useLocation<{ from: { pathname: string } }>();
 
+  const { register, handleSubmit, errors } = useForm();
+
   const { from } = location.state || { from: { pathname: "/links" } };
-  const performLogin = () => {
+  const onSubmit = ({ email, password }: { email: string, password: string}) => {
     setIsLoading(true);
     login({ variables: { email, password } })
       .then((result) => {
@@ -37,6 +38,7 @@ export const Login = ({ setIsAuthenticated }: { setIsAuthenticated: (state: bool
         history.replace(from);
       })
       .catch((e) => {
+        console.log(e);
         if (e && e.message) {
           setError(e.message);
         } else {
@@ -49,6 +51,7 @@ export const Login = ({ setIsAuthenticated }: { setIsAuthenticated: (state: bool
 
   return (
     <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
       <h1>Login</h1>
 
       <div className="mb-3 row">
@@ -60,12 +63,11 @@ export const Login = ({ setIsAuthenticated }: { setIsAuthenticated: (state: bool
             id="email"
             type="text"
             name="email"
-            value={email}
-            className="form-control"
-            onChange={(event) => {
-              setEmail(event.target.value);
-            }}
+            className={classnames("form-control", { "is-invalid": errors.email })}
+            ref={register({ required: true })}
+            defaultValue={""}
           />
+          {errors.email && <div className=" invalid-feedback ">Please provide an email address</div> }
         </div>
       </div>
       <div className="mb-3 row">
@@ -77,12 +79,11 @@ export const Login = ({ setIsAuthenticated }: { setIsAuthenticated: (state: bool
             id="password"
             type="password"
             name="password"
-            value={password}
-            className="form-control"
-            onChange={(event) => {
-              setPassword(event.target.value);
-            }}
+            className={classnames("form-control", { "is-invalid": errors.password })}
+            ref={register({ required: true })}
+            defaultValue={""}
           />
+          {errors.password && <div className="invalid-feedback">Please provide a password.</div> }
         </div>
       </div>
 
@@ -92,12 +93,11 @@ export const Login = ({ setIsAuthenticated }: { setIsAuthenticated: (state: bool
       <button
         className="btn btn-primary"
         name="loginButton"
-        onClick={() => {
-          performLogin();
-        }}
+        type="submit"
       >
         Login
       </button>
+      </form>
     </div>
   );
 };
