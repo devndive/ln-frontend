5cb6052720a950a9cea964dec711fc307e768389
Yann Duval <yann.duval@posteo.de>
2022-07-26 12:18:58 +0200

    Updated dependencies manually

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { App } from "./App";
 import { AuthProvider } from "./AuthProvider";
